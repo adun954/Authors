7aca38cb28686c7ea4e2797845e411be043d22c5
adun954 <allendun954@gmail.com>
2022-11-12 21:53:27 -0800

    Authors

--- src/components/AuthorForm.js
+++ src/components/AuthorForm.js
@@ -7,11 +7,9 @@ import { Link, useNavigate } from 'react-router-dom';
 const Author = (props) => {
     const navigate = useNavigate();
     const [name, setName] = useState("");
-    const [errors, setErrors] = useState("");
     
-    
+    const [errors, setErrors] = useState([]);
     const onSubmitHandler = e => {
-
         e.preventDefault();
         // make a post request to create a new authors
         axios.post('http://localhost:8000/api/authors/new', {
@@ -27,25 +25,35 @@ const Author = (props) => {
         // (catch) : if it does not succefully return the response of the api, return a error
         // Put a validation after "err" to state the name must be 3 characters or more
         .catch(err=>{
-            const errorResponse = err.response.data.errors; // Get the errors from err.response.data
-            const errorArr = []; // Define a temp error array to push the messages in
-            for (const key of Object.keys(errorResponse)) { // Loop through all errors and get the messages
-                errorArr.push(errorResponse[key].message)
-            }
-            // Set Errors
-            setErrors(errorArr);
+            console.log(err)
+            // to find errorResponse you must first -> Console ->
+            // message properties -> find the objects the access to the 
+            // validations
+            const errorResponse = err.response.data.errors.name; // Get the errors from err.response.data
+                const errorArr = []; // Define a temp error array to push the messages in
+                for (const key of Object.keys(errorResponse)) { // Loop through all errors and get the messages
+                    errorArr.push(errorResponse[key].message)
+                }
+                // Set Errors
+                setErrors(errorArr);
         })
     };
     
     const handleCancel = (e) => {
         navigate('/authors');
     }
-    return(
+    
+    return (
+        
+
         <form onSubmit={ onSubmitHandler }>
         {errors.map((err, index) => <p key={index}>{err}</p>)}
+        
             <div>
                 <label>Name: </label> 
-                <input type="text" onChange={ (e) => setName(e.target.value) } /> 
+                <input type="text" 
+                value={name}
+                onChange={ (e) => setName(e.target.value) } /> 
             </div>
             
             <input type="submit" value="Submit" />
